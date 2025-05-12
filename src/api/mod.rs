d5f5d8d38e887437dd0731bf0f7cec6bb705d997
TomRomeo <tom.romeo.doil@gmail.com>
2025-05-12 14:49:01 +0200

    fix: authentication

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,11 +1,11 @@
 use crate::api::syncs::update_progress::DocumentProgress;
 use crate::db::Database;
 use poem::Result;
-use poem_openapi::auth::Basic;
 use poem_openapi::param::Path;
 use poem_openapi::payload::{Json, PlainText};
-use poem_openapi::{OpenApi, SecurityScheme};
+use poem_openapi::{OpenApi};
 use std::sync::Arc;
+use crate::api::users::auth::{ApiAuthentication};
 
 mod healthcheck;
 pub mod syncs;
@@ -21,9 +21,6 @@ impl Api {
     }
 }
 
-#[derive(SecurityScheme)]
-#[oai(ty = "basic")]
-pub struct BasicAuthentication(Basic);
 
 #[OpenApi]
 impl Api {
@@ -36,17 +33,17 @@ impl Api {
 
     /// Get progress
     #[oai(path = "/syncs/progress/:id", method = "get")]
-    async fn get_progress(&self, auth: BasicAuthentication, id: Path<String>) -> Result<Json<DocumentProgress>> {
-        let (username, _) = self.auth(auth).await?;
-        Ok(syncs::get_progress::handler(self.db.as_ref(), username, id.0).await?)
+    async fn get_progress(&self, auth: ApiAuthentication, id: Path<String>) -> Result<Json<DocumentProgress>> {
+        self.auth(&auth).await?;
+        Ok(syncs::get_progress::handler(self.db.as_ref(), auth.username, id.0).await?)
     }
 
     /// Update progress
     #[oai(path = "/syncs/progress", method = "post")]
-    async fn update_progress(&self, auth: BasicAuthentication, req: Json<DocumentProgress>) -> Result<PlainText<String>> {
-        let (username, _) = self.auth(auth).await?;
+    async fn update_progress(&self, auth: ApiAuthentication, req: Json<DocumentProgress>) -> Result<PlainText<String>> {
+        self.auth(&auth).await?;
         Ok(syncs::update_progress::handler(self.db.as_ref(),
-            &username,
+            &auth.username,
             &req.document,
             &req.percentage,
             &req.progress,
@@ -58,7 +55,15 @@ impl Api {
     /// Create user
     #[oai(path = "/users/create", method = "post")]
     async fn create_user(&self, req: Json<users::create::CreateUserRequest>) -> PlainText<String> {
+        println!("register endpoint hit");
         users::create::handler(self.db.as_ref(), &req.username, &req.password).await
+    }
+
+    // Auth user
+    #[oai(path = "/users/auth", method = "get")]
+    async fn auth_user(&self, auth: ApiAuthentication) -> Result<PlainText<String>> {
+        println!("auth endpoint hit");
+        Ok(users::auth::handler(self.db.as_ref(), &auth.username, &auth.password).await?)
     }
 
 }
--- src/api/users/auth.rs
+++ src/api/users/auth.rs
@@ -1,15 +1,44 @@
-use crate::api::BasicAuthentication;
 use poem::http::StatusCode;
-use poem::Error;
+use poem::{Error, FromRequest, RequestBody};
+use poem_openapi::payload::PlainText;
 
 impl crate::Api {
-    pub async fn auth(&self, auth: BasicAuthentication) -> Result<(String, String), Error> {
-        if auth.0.username == "" || auth.0.password == "" {
+    pub async fn auth(&self, auth: &ApiAuthentication) -> Result<(), Error> {
+        if auth.username == "" || auth.password == "" {
             return Err(Error::from_status(StatusCode::UNAUTHORIZED));
         }
-        self.db.validate_password(&auth.0.username, &auth.0.password)
+        self.db.validate_password(&auth.username, &auth.password)
             .await
             .map_err(|_| Error::from_status(StatusCode::UNAUTHORIZED))?;
-        Ok((auth.0.username, auth.0.password))
+        Ok(())
+    }
+}
+
+pub async fn handler(db: & dyn crate::db::Database, username: &str, password: &str) -> Result<PlainText<String>, Error> {
+
+    match db.validate_password(username, password).await {
+        Ok(..) => {
+            Ok(PlainText("User authenticated successfully".to_string()))
+        }
+        Err(err) => {
+            eprintln!("Failed to log in user: {}", err);
+            Err(Error::from_status(StatusCode::UNAUTHORIZED))
+        }
+    }
+}
+pub struct ApiAuthentication {
+    pub username: String,
+    pub password: String,
+}
+
+impl FromRequest<'_> for ApiAuthentication {
+    async fn from_request(req: &poem::Request, body: &mut RequestBody) -> poem::Result<Self> {
+        let username = req.headers().get("x-auth-user").and_then(|h| h.to_str().ok()).unwrap_or("");
+        let password = req.headers().get("x-auth-key").and_then(|h| h.to_str().ok()).unwrap_or("");
+
+        Ok(ApiAuthentication {
+            username: username.to_string(),
+            password: password.to_string(),
+        })
     }
 }
